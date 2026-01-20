630edf3e0c792f5f953ac6aeee968ba94fadb5b1
DESKTOP-111JM1H\Kit Barry <kit.barry@sheridancollege.ca>
2026-01-20 10:47:12 -0500

    Used an array and a list to swap the sprite

--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -1,23 +1,34 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 
 public class SpriteChanger : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Color col;
+    public List<Sprite> barrels;
+    public int randomNumber;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //PickARandomColour();
+        PickARandomSprite();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Keyboard.current.anyKey.wasPressedThisFrame == true)
-        //{
-        //    PickARandomColour();
-        //}
+        if (Keyboard.current.anyKey.wasPressedThisFrame == true)
+        {
+            Debug.Log("Try to change the sprite please");
+            //PickARandomColour();
+            if(barrels.Count > 0)
+            {
+                PickARandomSprite();
+            }
+            
+        }
 
         //NOT THIS:  spriteRenderer.sprite.bounds.Contains()
         //THIS: spriteRenderer.bouncs.Contains()
@@ -36,11 +47,25 @@ public class SpriteChanger : MonoBehaviour
             spriteRenderer.color = Color.white;
         }
 
-
+        if(Mouse.current.leftButton.wasPressedThisFrame == true && barrels.Count > 0)
+        {
+            barrels.RemoveAt(0);
+        }
     }
 
     void PickARandomColour()
     {
         spriteRenderer.color = Random.ColorHSV();
+    }
+
+    void PickARandomSprite()
+    {
+        //spriteRenderer.sprite = mySprite;
+
+        //pick a random number
+        randomNumber = Random.Range(0, barrels.Count);
+        //use that number to choose a sprite
+        //assign that sprite to the sprite renderer
+        spriteRenderer.sprite = barrels[randomNumber];
     }
 }
